00521f890affe4c663f1c70652e56df1d72bbf0c
Georgi <georgy.keranov@gmail.com>
2021-11-13 01:21:49 +0200

    Add publish of items and vehicles in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from helpers.scraper import Scraper
+from helpers.csv_helper import get_data_from_csv
+from helpers.listing_helper import publish_listing
 
 scraper = Scraper('https://facebook.com')
 
@@ -8,3 +10,18 @@ scraper.add_login_functionality('https://facebook.com', 'a[href="/me/"]', 'faceb
 
 # Go to the create new listing page
 scraper.element_click('a[href="https://www.facebook.com/marketplace/?ref=bookmark"]')
+
+# Publish items
+items = get_data_from_csv('items')
+if items:
+	for item in items:
+		publish_listing(item, 'item', scraper)
+
+# Publish vehicles
+vehicles = get_data_from_csv('vehicles')
+if vehicles:
+	for vehicle in vehicles:
+		publish_listing(vehicle, 'vehicle', scraper)
+
+# Wait some time before closing the Chrome
+time.sleep(15)
