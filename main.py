951809904e0d81fc0ca0f6f8c8bbba73b4c62b84
Georgi <georgy.keranov@gmail.com>
2021-11-13 15:51:25 +0200

    Refactor code in main.py

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import time
 from helpers.scraper import Scraper
 from helpers.csv_helper import get_data_from_csv
+from helpers.listing_helper import remove_listing
 from helpers.listing_helper import publish_listing
+
+# Get data from csv then remove listings if already exist and finaly publish the listings into the facebook marketplace
+def get_data_and_publish_listings(file_name, listings_type, scraper):
+	# Get data from csv file
+	listings = get_data_from_csv(file_name)
+
+	# If data is empty stop the function
+	if not listings:
+		return
+
+	# Check if listing is already listed and remove it then publish it like a new one
+	for listing in listings:
+		# Remove listing if it is already published
+		remove_listing(listing, listings_type, scraper)
+
+		# Publish the listing in marketplace
+		publish_listing(listing, listings_type, scraper)
 
 scraper = Scraper('https://facebook.com')
 
 # Add login functionality to the scraper
 scraper.add_login_functionality('https://facebook.com', 'a[href="/me/"]', 'facebook')
 
-# Go to the create new listing page
+# Go to the marketplace page
 scraper.element_click('a[href="https://www.facebook.com/marketplace/?ref=bookmark"]')
 
-# Publish items
-items = get_data_from_csv('items')
-if items:
-	for item in items:
-		publish_listing(item, 'item', scraper)
+# Click on create new listing button
+scraper.element_click('div[aria-label="Marketplace sidebar"] a[aria-label="Create new listing"]')
 
-# Publish vehicles
-vehicles = get_data_from_csv('vehicles')
-if vehicles:
-	for vehicle in vehicles:
-		publish_listing(vehicle, 'vehicle', scraper)
+# Click on Your Listings button
+scraper.element_click('a[href="/marketplace/you/selling/"]:not([aria-current="page"])')
+
+# Publish all the vehicles from csvs/items.csv into the facebook marketplace
+get_data_and_publish_listings('items', 'item', scraper)
+
+# Publish all the vehicles from csvs/vehicles.csv into the facebook marketplace
+get_data_and_publish_listings('vehicles', 'vehicle', scraper)
 
 # Wait some time before closing the Chrome
 time.sleep(15)
