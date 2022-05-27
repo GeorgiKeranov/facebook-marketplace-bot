ac88f2feac5258dac9eb222141066bfb8b37b461
Georgi <georgy.keranov@gmail.com>
2022-05-27 21:15:27 +0300

    Remove not used import

--- helpers/scraper.py
+++ helpers/scraper.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import InvalidArgumentException
 from selenium.common.exceptions import ElementClickInterceptedException
 
